5a855c7ed6f074b007ea4e63e079fdd847356f6f
Zen Software <ZenSoftware@users.noreply.github.com>
2025-06-12 05:01:44 -0600

    feat(unity): added GraphQL typing support to .graphql
    undefined

---
+++ apps/zen-unity/apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'zen-apollo-unity',
+      url: 'http://localhost:7080/graphql',
+    },
+    includes: ['**/*.graphql'],
+    excludes: [],
+  },
+};
